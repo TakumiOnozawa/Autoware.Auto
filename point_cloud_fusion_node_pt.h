9a4f6528033c278598314a9ac5d53ed8cd0870a0
Onozawa Takumi <71485328+TakumiOnozawa@users.noreply.github.com>
2022-01-17 13:59:43 +0900

    Add files via upload

---
+++ point_cloud_fusion_node_pt.h
@@ -0,0 +1,4 @@
+unsigned long long pt_generate( float x_it_in, float y_it_in, float z_it_in );
+void add_point_x(unsigned long long pt,float x_it_in);
+void add_point_y(unsigned long long pt,float y_it_in);
+void add_point_z(unsigned long long pt,float z_it_in);
